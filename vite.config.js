b75f05011f7a0bc83280e993ae4f368398738b6d
SatyamRoy007 <satyam.roy02@gmail.com>
2026-07-22 09:02:45 +0530

    Fix CI build command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ 
-    plugins: [react()],
-    base: "/portfolio",
- });
-np
+export default defineConfig({ plugins: [react()] });
